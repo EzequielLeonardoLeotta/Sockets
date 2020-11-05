f4c44459d6ecd14359ec4896077822f819a265f2
Javier Rodriguez <pires_mdp@hotmail.com>
2020-11-05 12:45:30 -0300

    AltaServicio desde cliente con menu.

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -5,6 +5,9 @@
 #include <WS2tcpip.h>
 #include <fstream>
 #include <ctime>
+#include <string>
+#include <sstream>
+#include <iomanip>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -17,6 +20,8 @@ void verRegistro(SOCKET& sock);
 void menu(SOCKET& sock);
 int enviarMensaje(string& mensaje, SOCKET& sock);
 string recibirMensaje(SOCKET& sock);
+void mostrarAsientos(string& asientos);
+void mostrarServicio(string servicio);
 
 // Variables globales
 string usuario;
@@ -138,33 +143,113 @@ int main()
 
 // Implementaciones
 void altaServicio(SOCKET& sock) {
-	string origen, fecha, turno, alta, log;
+	string log;
 
+	// Pedir datos al usuario
 	system("cls");
 	cout << "Alta de servicio" << endl;
-	cout << "Ingrese origen: "; cin >> origen;
-	cout << "Ingrese Fecha: "; cin >> fecha;
-	cout << "Ingrese Turno: "; cin >> turno;
-	alta = "altaServicio;" + origen + ";" + fecha + ";" + turno + ";";
+
+	// Pedir origen
+	int origen = 0;
+	while (origen > 2 || origen < 1) {
+		cout << endl << "Origen: " << endl
+			<< "1- Mar del Plata" << endl
+			<< "2- Buenos Aires" << endl
+			<< endl << "Ingrese una opción: ";
+		cin >> origen;
+	}
+
+	// Pedir turno
+	int turno = 0;
+	while (turno > 3 || turno < 1) {
+		cout << endl << "Turno: " << endl
+			<< "1- Mañana" << endl
+			<< "2- Tarde" << endl
+			<< "3- Noche" << endl
+			<< endl << "Ingrese una opción: ";
+		cin >> turno;
+	}
+
+	// Pedir fecha
+	int dia = 0;
+	int mes = 0;
+	int anio = 0;
+	cout << endl << "Fecha: " << endl;
+	while (dia < 1 || dia>31) {
+		cout << "Ingrese el día (ej: 11): ";
+		cin >> dia;
+	}
+	while (mes < 1 || mes>12) {
+		cout << "Ingrese el mes (ej: Enero sería 1): ";
+		cin >> mes;
+	}
+	while (anio < 1900 || anio>2500) {
+		cout << "Ingrese el año (ej: 2020): ";
+		cin >> anio;
+	}
+
+	// Preparar datos en string
+	stringstream servicioS;
+	servicioS << "altaServicio;";
+
+	// Origen
+	switch (origen) {
+	case 1:
+		servicioS << "m;";
+		break;
+	case 2:
+		servicioS << "b;";
+		break;
+	}
+	// Turno
+	switch (turno) {
+	case 1:
+		servicioS << "m;";
+		break;
+	case 2:
+		servicioS << "t;";
+		break;
+	case 3:
+		servicioS << "n;";
+		break;
+	}
+	// Fecha
+	stringstream fechaS;
+	fechaS << std::setfill('0') << std::setw(2) << dia
+		<< std::setfill('0') << std::setw(2) << mes
+		<< anio << ";";
+	string fecha;
+	fechaS >> fecha;
+	servicioS << fecha;
+	string asientos = "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo;";
+	servicioS << asientos;
+	string alta;
+	servicioS >> alta;
 
 	if (enviarMensaje(alta, sock) != 1) {
-		while (recibirMensaje(sock) != "")
-		{
-			string res = recibirMensaje(sock);
-			if (res == "altaOk") {
-				cout << "Se genero con exito el serivicio";
+		string res = recibirMensaje(sock);
+		if (res == "altaOk") {
+			system("cls");
+
+			cout << "Se ha dado de alta el siguiente servicio: " << endl << endl;
+
+			// Quitarle el comando al alta
+			string delimitador = ";";
+			alta.erase(0, alta.find(delimitador) + delimitador.length());
+			// Mostrar el servicio
+			mostrarServicio(alta);
+
+			system("pause");
+		}
+
+		if (res == "altaNegada") {
+				cout << endl << "Error: El servicio se repite o no cumple con las condiciones requeridas" << endl << endl;
+
 				system("pause");
-			}
-			
-			if (res == "altaNegada") {
-					cout << "El servicio se repite o no cumple con las condiciones requeridas";
-					system("pause");
-			}
 		}
 	}
-	else {
+	else 
 		cout << "Error al enviar mensaje";
-	}
 }
 
 void gestionPasaje() {
@@ -261,4 +346,78 @@ string recibirMensaje(SOCKET& sock) {
 	respuesta.assign(bufer);
 
 	return respuesta;
+}
+
+void mostrarAsientos(string& asientos) {
+	// Columnas
+	cout << "Asientos:" << endl
+		<< "   | ";
+	for (size_t i = 1; i <= 20; i++) {
+		cout << setw(2) << i << " ";
+	}
+	cout << endl;
+
+	// Separador
+	cout << "-----------------------------------------------------------------" << endl;
+
+	// Fila A
+	cout << " A | ";
+	for (size_t i = 0; i < 20; i++) {
+		cout << setw(2) << asientos[i] << " ";
+	}
+	cout << endl;
+
+	// Fila B
+	cout << " B | ";
+	for (size_t i = 20; i < 40; i++) {
+		cout << setw(2) << asientos[i] << " ";
+	}
+	cout << endl;
+
+	// Separador "Pasillo"
+	cout << "   | " << "============================================================" << endl;
+
+	// Fila C
+	cout << " C | ";
+	for (size_t i = 40; i < 60; i++) {
+		cout << setw(2) << asientos[i] << " ";
+	}
+	cout << endl << endl;
+}
+
+void mostrarServicio(string servicio) {
+	// Partir el registro en campos
+	string delimitador = ";";
+	string origen = servicio.substr(0, servicio.find(";"));
+	servicio.erase(0, servicio.find(delimitador) + delimitador.length());
+	string turno = servicio.substr(0, servicio.find(";"));
+	servicio.erase(0, servicio.find(delimitador) + delimitador.length());
+	string fecha = servicio.substr(0, servicio.find(";"));
+	servicio.erase(0, servicio.find(delimitador) + delimitador.length());
+	string asientos = servicio.substr(0, servicio.find(";"));
+
+	// Dar formato al registro para mostrar por consola
+	// Origen
+	if (origen == "m")
+		origen = "Mar del Plata";
+	else
+		origen = "Buenos Aires";
+	// Turno
+	if (turno == "m")
+		turno = "Mañana";
+	else if (turno == "t")
+		turno = "Tarde";
+	else
+		turno = "Noche";
+	// Fecha
+	string separador = "/";
+	fecha.insert(2, separador);
+	fecha.insert(5, separador);
+
+	cout << "Origen: " << origen << endl
+		<< "Turno: " << turno << endl
+		<< "Fecha: " << fecha << endl;
+
+	// Mostrar asientos
+	mostrarAsientos(asientos);
 }
